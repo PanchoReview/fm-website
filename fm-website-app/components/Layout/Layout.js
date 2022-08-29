1ebd3a3b0ea79300a8a96ef37bb92500fb5ef1f0
Francisco Astorga <fastorga@nimok.com>
2022-08-29 12:15:15 -0400

    Añadido control ScrollToTop

--- fm-website-app/components/Layout/Layout.js
+++ fm-website-app/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import FunctionalMovementNavbar from '../Navbar/FunctionalMovementNavbar'
+import ScrollToTop from '../ScrollToTop/ScrollToTop'
 
 const Layout = ({ children }) => {
     return(
         <>
             <FunctionalMovementNavbar />
             {children}
+            <ScrollToTop />
         </>
     )
 }
---
+++ fm-website-app/components/ScrollToTop/ScrollToTop.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import { ChevronUp } from "react-iconly"
+import { Button } from "@nextui-org/react"
+import classNames from "../../utils/classNames"
+import styles from "./ScrollToTop.module.css"
+
+const ScrollToTop = () => {
+    const [isVisible, setIsVisible] = useState(false)
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setIsVisible(true)
+        }
+        else{
+            setIsVisible(false)
+        }
+    }
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility)
+
+        return () => {
+            window.removeEventListener("scroll", toggleVisibility)
+        }
+    }, [])
+
+    return(
+        <div className={styles.scrollToTopContainer}>
+            <Button rounded auto disabled={!isVisible}
+                icon={<ChevronUp set="bold" filled className={styles.icon} />} 
+                onClick={scrollToTop}
+                className={classNames(isVisible ? styles.show : styles.hide, styles.scrollToTopBtn)} />            
+        </div>
+    )
+}
+
+export default ScrollToTop
